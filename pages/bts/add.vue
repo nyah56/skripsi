9afcc04b1c3006697b86a0347cc0722f64c9d743
nyah56 <x6perb@gmail.com>
2023-11-27 07:39:00 +0700

    Style Scoped

--- pages/bts/add.vue
+++ pages/bts/add.vue
@@ -1,17 +1,13 @@
 <template>
+  <NuxtLink to="/bts/">Back</NuxtLink>
+
+  <v-col cols="2">
+    <v-label class="font-weight-bold mb-1">Pilih dari Peta</v-label>
+  </v-col>
   <div class="container elevation-4">
     <h2 class="mb-5">Tambah Data BTS</h2>
     <form @submit.prevent="handleSubmit">
       <v-row class="d-flex mb-3">
-        <v-col cols="12">
-          <v-label class="font-weight-bold mb-1">ID BTS</v-label>
-          <v-text-field
-            variant="outlined"
-            hide-details
-            color="primary"
-            v-model="id"
-          ></v-text-field>
-        </v-col>
         <v-col cols="12">
           <v-label class="font-weight-bold mb-1">Nama BTS</v-label>
           <v-text-field
@@ -57,82 +53,131 @@
             color="primary"
           ></v-text-field>
         </v-col>
-        <v-col cols="4" class="pt-0">
+        <v-col cols="5">
+          <v-label class="font-weight-bold mb-1">Latitude</v-label>
+          <v-text-field
+            variant="outlined"
+            hide-details
+            color="primary"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="5">
+          <v-label class="font-weight-bold mb-1">Longitude</v-label>
+          <v-text-field
+            variant="outlined"
+            hide-details
+            color="primary"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-label class="font-weight-bold mb-1">Pilih dari Peta</v-label>
+          <v-btn color="secondary" @click="toggle"><MapIcon></MapIcon></v-btn>
+        </v-col>
+        <mapModal v-if="isShow"></mapModal>
+        <v-col cols="4" class="pt-0" offset="8">
           <v-btn
             class="mt-4"
             color="primary"
             size="large"
             block
             @click="handleSubmit"
-            >Submit</v-btn
-          >
+            ><span v-if="!isLoad">Submit</span>
+            <v-progress-circular indeterminate v-else></v-progress-circular>
+          </v-btn>
         </v-col>
       </v-row>
     </form>
   </div>
 </template>
 
-<script>
-import { ref, inject } from 'vue';
-import { collection, addDoc } from 'firebase/firestore'; // Make sure to import the necessary Firestore functions
+<script setup>
+import Button from '@/components/bts/Button.vue';
+import mapModal from '@/components/bts/mapModal.vue';
+import { MapIcon } from 'vue-tabler-icons';
+import { ref, inject, onMounted } from 'vue';
+import { collection, addDoc, getDocs } from 'firebase/firestore'; // Make sure to import the necessary Firestore functions
+const router = useRouter();
+const firestore = inject('firestore'); // Assuming you have a Nuxt plugin that provides Firestore
+const btsCollection = collection(firestore, 'bts');
 
-export default {
-  setup() {
-    const firestore = inject('firestore'); // Assuming you have a Nuxt plugin that provides Firestore
+const isLoad = ref(false);
+const isShow = ref(false);
+const nama = ref('');
+const alamat = ref('');
+const kapasitas = ref('');
+const jml_pelanggan = ref('');
+const layanan_terpakai = ref('');
+const toggle = () => {
+  isShow.value = !isShow.value;
+};
+const getLastData = async () => {
+  try {
+    const data = [];
+    const querySnapshot = await getDocs(btsCollection);
 
-    const id = ref('');
-    const nama = ref('');
-    const alamat = ref('');
-    const kapasitas = ref('');
-    const jml_pelanggan = ref('');
-    const layanan_terpakai = ref('');
+    querySnapshot.forEach((doc) => {
+      // Here, you can access the document data
+      data.push(doc.data());
+    });
 
-    const handleSubmit = async () => {
-      // Get a Firestore reference to the 'bts' collection
-      const btsCollection = collection(firestore, 'bts');
-
-      // Create a separate object with id and nama properties
-      const dataObject = {
-        id_bts: id.value,
-        nama_bts: nama.value,
-        alamat: alamat.value,
-        kapasitas: parseInt(kapasitas.value),
-        layanan_terpakai: parseInt(layanan_terpakai.value),
-        jml_pelanggan: parseInt(jml_pelanggan.value),
+    const idCount = data.map((item) => {
+      const { id_bts } = item;
+      const numericPart = parseInt(id_bts.slice(3), 10);
+      return {
+        numericPart,
       };
+    });
 
-      try {
-        // Add a new document to the 'bts' collection with the input data
-        await addDoc(btsCollection, dataObject);
+    // btsData.value = modifiedData;
+    const lastID = idCount.sort((b, a) => a.numericPart - b.numericPart);
+    // const newId = 'bts' + lastID[0].numericPart.toString();
+    const newId = lastID[0].numericPart + 1;
+    const idString = 'bts' + newId;
+    // console.log(typeof idString);
+    // console.table(btsData.value);
+    // isLoad.value = false;
+    // console.log('array', btsData.value);
+    return idString;
+  } catch (error) {
+    console.error('Error getting data:', error);
+  }
+};
+const handleSubmit = async () => {
+  // Get a Firestore reference to the 'bts' collection
+  const btsCollection = collection(firestore, 'bts');
+  // id.value = await getLastData();
+  // console.log(id.value);
+  // Create a separate object with id and nama properties
+  isLoad.value = true;
+  const dataObject = {
+    id_bts: await getLastData(),
+    nama_bts: nama.value,
+    alamat: alamat.value,
+    kapasitas: parseInt(kapasitas.value),
+    layanan_terpakai: parseInt(layanan_terpakai.value),
+    jml_pelanggan: parseInt(jml_pelanggan.value),
+  };
 
-        // Optionally, you can reset the form fields after successful submission
-        id.value = '';
-        nama.value = '';
-        alamat.value = '';
-        kapasitas.value = '';
-        layanan_terpakai.value = '';
-        jml_pelanggan.value = '';
+  try {
+    // Add a new document to the 'bts' collection with the input data
+    await addDoc(btsCollection, dataObject);
 
-        console.log('Data successfully sent to Firestore:', dataObject);
-      } catch (error) {
-        console.error('Error sending data to Firestore:', error.message);
-      }
-    };
+    // Optionally, you can reset the form fields after successful submission
 
-    return {
-      id,
-      nama,
-      alamat,
-      kapasitas,
-      layanan_terpakai,
-      jml_pelanggan,
-      handleSubmit,
-    };
-  },
+    nama.value = '';
+    alamat.value = '';
+    kapasitas.value = '';
+    layanan_terpakai.value = '';
+    jml_pelanggan.value = '';
+    router.push('/bts/');
+    console.log('Data successfully sent to Firestore:', dataObject);
+  } catch (error) {
+    console.error('Error sending data to Firestore:', error.message);
+  }
 };
 </script>
 
-<style>
+<style scoped>
 .container {
   padding: 20px;
   border-radius: 10px;
